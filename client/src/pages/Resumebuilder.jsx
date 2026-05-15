69a71769bb384b8106fa0e689f64da39b0df6b50
adi-builds <adityapaul500@gmail.com>
2026-05-15 20:52:05 +0530

    50% resumebuilder page

--- client/src/pages/Resumebuilder.jsx
+++ client/src/pages/Resumebuilder.jsx
@@ -1,8 +1,86 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { Link,useParams } from 'react-router-dom'
+import { ArrowLeftIcon, Briefcase, ChevronLeft, FileText, FolderIcon, GraduationCap, Sparkle, User } from 'lucide-react'
 const Resumebuilder = () => {
+
+  const { resumeId } =useParams()
+
+  const [resumeData, setResumeData] = useState({
+    _id:'',
+    title: '',
+    professional_info: {},
+    professional_summary: '',
+    experience: [],
+    education: [],
+    projects: [],
+    skills: [],
+    templates: "classic",
+    accent_color: "#3B82F6",
+    public: false,
+  })
+
+  const loadExistingResume = async()=>{
+    const resume = dummyResumee.find(resume => resume._id === resumeId)
+    if (resume) {
+      setResumeData(resume)
+      document.title = resume.title
+    }
+  }
+
+  useEffect(()=>{
+    loadExistingResume()
+  }, [])
+
+
+  const [activeSectionIndex,setActiveSectionIndex]= useState(0)
+  const [removeBackground, setRemoveBackground] = useState(false)
+
+  const sections =[
+    {id:"personal", name:"Personal Info", icon: User},
+    {id:"summary", name:"Summary", icon: FileText},
+    {id:"experience", name:"Experience", icon: Briefcase},
+    {id:"education", name:"Education", icon: GraduationCap},
+    {id:"projects", name:"Projects", icon: FolderIcon},
+    {id:"skills", name:"Skills", icon: Sparkle},
+  ]
+
+  const activeSection = sections[activeSectionIndex]
+
   return (
-    <div><h1>Resumebuilder</h1></div>
+    <div>
+      <div className='max-w-7xl mx-auto px-4 py-6'>
+        <Link to={'/app'} className='inline-flex gap-2 items-center text-slate-500 hover:text-slate-700 transition-all'>
+        <ArrowLeftIcon className='size-4'/> Back to Dashboard
+        </Link>
+      </div>
+
+      <div className='max-w-7xl mx-auto px-4 pb-8'>
+        <div className='grid lg:grid-cols-12 gap-8'>
+           {/* Left Panel - Form */}
+           <div className='relative lg:col-span-5 rounded-lg overflow-hidden'>
+            <div className='bg-white rounded-lg shadow-sm border border-gray-200 p-6 pt-1'></div>
+            {/* progress bar using activesectionindex */}
+            <hr className='absolute top-0 left-0 right-0 border-2 border-gray-200' />
+            <hr className='absolute top-0 left-0 h-1 bg-gradient-to-r from-green-500 to-green-600 border-none transition-all duration-2000' style={{width:`${activeSectionIndex * 100/(sections.length-1) }%`}} />
+           </div>
+
+           {/*Section Navigation*/}
+           <div className='flex justify-between items-center mb-6 border-b border-gray-300 py-1'>
+            <div></div>
+            <div className='flex items-center'>
+              {activeSectionIndex !== 0 &&(
+                <button className='flex items-center gap-1 p-3 rounded-lg text-sm font-medium text-gray-600 hover:bg-gray-50 transition-all'>
+                  <ChevronLeft className='size-4'/> Previous
+                </button>
+              )}
+            </div>
+
+            {/* Right Panel - Resume Preview */}
+            <div></div>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
