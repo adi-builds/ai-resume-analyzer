f22af6ad2e86aa586ff0d6c8fd8eeba87d8b5e5c
adi-builds <adityapaul500@gmail.com>
2026-05-17 00:57:01 +0530

    added summary form

--- client/src/pages/Resumebuilder.jsx
+++ client/src/pages/Resumebuilder.jsx
@@ -6,6 +6,7 @@ import { dummyResumeData } from '../assets/assets'
 import ResumePreview from '../components/ResumePreview'
 import TemplateSelector from '../components/TemplateSelector'
 import ColorPicker from '../components/ColorPicker'
+import { ProfessionalSummaryForm } from '../components/ProfessionalSummaryForm'
 
 const Resumebuilder = () => {
 
@@ -104,6 +105,9 @@ const Resumebuilder = () => {
                     removeBackground={removeBackground}
                     setRemoveBackground={setRemoveBackground}
                   />
+                )}
+                {activeSection.id === 'summary' &&(
+                  <ProfessionalSummaryForm data={resumeData.professional_summary} onChange={(data)=>setResumeData(prev=>({...prev,professional_summary:data}))} setResumeData={setResumeData}/>
                 )}
               </div>
 
