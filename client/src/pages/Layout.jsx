2e7f75f13dc07554c1d1899ed7115d0b46b36a82
adi-builds <adityapaul500@gmail.com>
2026-06-21 13:48:49 +0530

    added login logic

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
+import { useSelector } from 'react-redux'
+import Loader from '../components/Loader'
+import Login from './Login'
 
 const Layout = () => {
+
+  const {user,loading} = useSelector(state =>state.auth)
+
+  if (loading) {
+    return <Loader/>
+  }
+
   return (
     <div>
-      <h1></h1>
-      <div>
+      {
+        user ? (
+          <div>
         <Navbar/>
         <Outlet/>
       </div>
+        ) :(
+          <Login/>
+        )
+      }
     </div>
   )
 }
